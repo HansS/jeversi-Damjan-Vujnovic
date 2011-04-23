2e9e48d4a04ccbd23569b9e506077d2c9c3a59b1
Gojko Adzic <nospam@gojko.com>
2011-04-23 23:35:07 +0100

    initial events

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -5,7 +5,17 @@ describe("Game", function() {
 		game = createGame();
 	});
 
-	it("should start with no events", function() {
-		expect(game.eventCount()).toEqual(0);
+	it("should start with four initial allocation events", function() {
+		expect(game.eventCount()).toEqual(5);
+		expect(game.getEvents()).toEqual([
+			createEvent("take","white",4,4),
+			createEvent("take","white",5,5),
+			createEvent("take","black",5,4),
+			createEvent("take","black",4,5),
+			createEvent("next","white")]);
+	});
+	it("should not allow clients to modify events", function(){
+		game.getEvents().push(createEvent("draw"));
+		expect(game.eventCount()).toEqual(5);
 	});
   });
--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,28 @@
+function createEvent(eventType, token, row, column) {
+	var result = {
+		eventType: eventType,
+		token: token,
+		row: row,
+		column: column
+	};
+	return result;
+};
 function createGame() {
 	var _eventCount=0;
-	return {
+	var events=[
+                 createEvent("take","white",4,4),
+                 createEvent("take","white",5,5),
+                 createEvent("take","black",5,4),
+                 createEvent("take","black",4,5),
+                 createEvent("next","white")];
+	var result={
 		eventCount: function(){
-			return _eventCount;
-		} 
+			return events.length;
+		},
+		getEvents: function () {
+			return events.slice(0);
+		}
 	}
+	return result;
 };
 
