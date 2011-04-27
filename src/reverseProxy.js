f94110416770b611bafa2825fcca524c945eed27
Gojko Adzic <nospam@gojko.com>
2011-04-27 19:08:54 +0100

    removed debug from reverseproxy

--- src/reverseProxy.js
+++ src/reverseProxy.js
@@ -3,7 +3,6 @@ jeversi.createReverseProxy = function (game, token, socket) {
 		socket.send(event);
 	});
 	socket.on("message", function (event) {
-		console.log("invoking", event.type, token);
 		game[event.type](token, event.row, event.column);
 	});
 	socket.send(jeversi.createEvent("init", token));
