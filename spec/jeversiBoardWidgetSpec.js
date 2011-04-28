c171a00f2afe70d5fbc60e137f9fb1602bb3abeb
Damjan Vujnovic <damjan@8pointers.com>
2011-04-28 10:39:07 +0100

    jeversiBoardWidgetSpec - minor cleanup

--- spec/jeversiBoardWidgetSpec.js
+++ spec/jeversiBoardWidgetSpec.js
@@ -2,48 +2,50 @@ describe("jeversiBoardWidget", function () {
 	var proxy = observable({
 		place: function (token, row, column) {
 		}
-	});
+	}), dispatchEvent = function () {
+		proxy.dispatchEvent("EventReceived", jeversi.createEvent.apply(jeversi, arguments));
+	}, widget, retrieveButton = function (row, column) {
+		return $(widget.find(":button")[8 * (row - 1) + column - 1]);
+	};
 	beforeEach(function () {
-		$("#jeversiBoardWidget").jeversiBoardWidget(proxy);
+		(widget = $("#jeversiBoardWidget")).jeversiBoardWidget(proxy);
 	});
 	it("should show own token when init event is received", function () {
-		proxy.dispatchEvent("EventReceived", jeversi.createEvent("init", "black"));
-		expect($("#jeversiBoardWidget").find(".token").text()).toBe("black");
+		dispatchEvent("init", "black");
+		expect(widget.find(".token").text()).toBe("black");
 	});
 	it("should show 'New game started' status when start event is received", function () {
-		proxy.dispatchEvent("EventReceived", jeversi.createEvent("start"));
-		expect($("#jeversiBoardWidget").find(".status").text()).toBe("New game started");
+		dispatchEvent("start");
+		expect(widget.find(".status").text()).toBe("New game started");
 	});
 	it("should reset all the buttons when start event is received", function () {
-		proxy.dispatchEvent("EventReceived", jeversi.createEvent("start"));
-		expect($($("#jeversiBoardWidget").find(":button")).val()).toBe("-");
+		dispatchEvent("start");
+		expect($(widget.find(":button")).val()).toBe("-");
 	});
 	it("should update the right buttons when take event is received", function () {
-		proxy.dispatchEvent("EventReceived", jeversi.createEvent("take", "white", 2, 3));
-		var button_2_3 = $($("#jeversiBoardWidget").find(":button")[10]);
-		expect(button_2_3.val()).toBe("w");
+		dispatchEvent("take", "white", 2, 3);
+		expect(retrieveButton(2, 3).val()).toBe("w");
 	});
 	it("should show 'white|black is next' status when next event is received", function () {
-		proxy.dispatchEvent("EventReceived", jeversi.createEvent("next", "black"));
-		expect($("#jeversiBoardWidget").find(".status").text()).toBe("black is next");
+		dispatchEvent("next", "black");
+		expect(widget.find(".status").text()).toBe("black is next");
 	});
 	it("should show 'invalid move by white|black' status when reject event is received", function () {
-		proxy.dispatchEvent("EventReceived", jeversi.createEvent("reject", "white"));
-		expect($("#jeversiBoardWidget").find(".status").text()).toBe("invalid move by white");
+		dispatchEvent("reject", "white");
+		expect(widget.find(".status").text()).toBe("invalid move by white");
 	});
 	it("should show 'invalid move by white|black' status when reject event is received", function () {
-		proxy.dispatchEvent("EventReceived", jeversi.createEvent("reject", "white"));
-		expect($("#jeversiBoardWidget").find(".status").text()).toBe("invalid move by white");
+		dispatchEvent("reject", "white");
+		expect(widget.find(".status").text()).toBe("invalid move by white");
 	});
-	it("should show 'game over. winner: white|black' status when finish event is received", function () {
-		proxy.dispatchEvent("EventReceived", jeversi.createEvent("finish", "black"));
-		expect($("#jeversiBoardWidget").find(".status").text()).toBe("game over. winner:black");
+	it("should show 'game over. winner:white|black' status when finish event is received", function () {
+		dispatchEvent("finish", "black");
+		expect(widget.find(".status").text()).toBe("game over. winner:black");
 	});
 	it("should invoke proxy.place when button is clicked", function () {
-		proxy.dispatchEvent("EventReceived", jeversi.createEvent("init", "black"));
+		dispatchEvent("init", "black");
 		spyOn(proxy, "place");
-		var button_2_3 = $($("#jeversiBoardWidget").find(":button")[10]);
-		button_2_3.click();
+		retrieveButton(2, 3).click();
 		expect(proxy.place).toHaveBeenCalledWith("black", 2, 3);
 	});
 });
